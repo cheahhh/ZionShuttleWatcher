64296578e8692e1c2f34cd3c7bfdf542cc7f4fbc
CONSERVICE\ivancheah <ivancheah@conservice.com>
2021-05-05 13:19:22 -0600

    lol

---
+++ Zion Shuttle Monitor/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Zion_Shuttle_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dt_startDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dt_endDate = new System.Windows.Forms.DateTimePicker();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_end = new System.Windows.Forms.Button();
+            this.txt_log = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pb_cooldown = new System.Windows.Forms.ProgressBar();
+            this.tmr_cooldown = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // dt_startDate
+            // 
+            this.dt_startDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dt_startDate.Location = new System.Drawing.Point(85, 12);
+            this.dt_startDate.Name = "dt_startDate";
+            this.dt_startDate.Size = new System.Drawing.Size(119, 23);
+            this.dt_startDate.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Begin Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "End Date:";
+            // 
+            // dt_endDate
+            // 
+            this.dt_endDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dt_endDate.Location = new System.Drawing.Point(273, 12);
+            this.dt_endDate.Name = "dt_endDate";
+            this.dt_endDate.Size = new System.Drawing.Size(119, 23);
+            this.dt_endDate.TabIndex = 3;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(398, 12);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(95, 23);
+            this.btn_start.TabIndex = 4;
+            this.btn_start.Text = "Begin Search";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_end
+            // 
+            this.btn_end.Enabled = false;
+            this.btn_end.Location = new System.Drawing.Point(499, 12);
+            this.btn_end.Name = "btn_end";
+            this.btn_end.Size = new System.Drawing.Size(95, 23);
+            this.btn_end.TabIndex = 5;
+            this.btn_end.Text = "Stop Search";
+            this.btn_end.UseVisualStyleBackColor = true;
+            this.btn_end.Click += new System.EventHandler(this.btn_end_Click);
+            // 
+            // txt_log
+            // 
+            this.txt_log.Location = new System.Drawing.Point(13, 41);
+            this.txt_log.Multiline = true;
+            this.txt_log.Name = "txt_log";
+            this.txt_log.ReadOnly = true;
+            this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_log.Size = new System.Drawing.Size(581, 425);
+            this.txt_log.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 472);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cool down period:";
+            // 
+            // pb_cooldown
+            // 
+            this.pb_cooldown.Location = new System.Drawing.Point(124, 472);
+            this.pb_cooldown.Maximum = 60;
+            this.pb_cooldown.Name = "pb_cooldown";
+            this.pb_cooldown.Size = new System.Drawing.Size(470, 23);
+            this.pb_cooldown.TabIndex = 8;
+            // 
+            // tmr_cooldown
+            // 
+            this.tmr_cooldown.Interval = 1000;
+            this.tmr_cooldown.Tick += new System.EventHandler(this.tmr_cooldown_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 508);
+            this.Controls.Add(this.pb_cooldown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_log);
+            this.Controls.Add(this.btn_end);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.dt_endDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dt_startDate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zion Shuttle Tickets Monitor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dt_startDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dt_endDate;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_end;
+        private System.Windows.Forms.TextBox txt_log;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar pb_cooldown;
+        private System.Windows.Forms.Timer tmr_cooldown;
+    }
+}
+
---
+++ Zion Shuttle Monitor/Form1.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zion_Shuttle_Monitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tmr_cooldown_Tick(object sender, EventArgs e)
+        {
+            pb_cooldown.Value += 1;
+            if (pb_cooldown.Value == pb_cooldown.Maximum)
+            {
+                pb_cooldown.Value = 0;
+                tmr_cooldown.Stop();
+                CheckForAvailableSlots();
+            }
+        }
+        private void CheckForAvailableSlots()
+        {
+            int campgroundId = 300016;
+            DateTime startDate = DateTime.Parse(dt_startDate.Value.ToShortDateString());
+            DateTime endDate = DateTime.Parse(dt_endDate.Value.ToShortDateString());
+            List<DateTime> datesToCamp = Enumerable.Range(0, 1 + endDate.Subtract(startDate).Days).Select(offset => startDate.AddDays(offset)).ToList();
+            WebClient client = new WebClient();
+            string urlBuilder = $"https://www.recreation.gov/api/ticket/availability/facility/{campgroundId}/monthlyAvailabilitySummaryView?year=2021&month=0{startDate.Month}&inventoryBucket=FIT";
+            string response = client.DownloadString(urlBuilder);
+            dynamic drecord = JsonConvert.DeserializeObject<dynamic>(response);
+            JObject campsiteRecords = drecord["facility_availability_summary_view_by_local_date"];
+            var hasSlots = false;
+            foreach (var item in campsiteRecords)
+            {
+                DateTime dateToCheck = DateTime.Parse(item.Key);
+                if (!datesToCamp.Contains(dateToCheck))
+                {
+                    continue;
+                }
+                List<string> dateSlots = item.Value.ToString().Split('\n').ToList();
+                if (!dateSlots.Contains("      \"has_reservable\": false,\r"))
+                {
+                    logText(item.Key + " has available slots");
+                    hasSlots = true;
+                }
+            }
+            if (!hasSlots)
+            {
+                logText("no slots found.");
+            }
+            tmr_cooldown.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForAvailableSlots();
+            btn_end.Enabled = true;
+            btn_start.Enabled = false;
+        }
+        private void logText(string item)
+        {
+            txt_log.Text +=  DateTime.Now.ToLongTimeString() + ": " + item + "\r\n";
+            txt_log.SelectionStart = txt_log.Text.Length;
+            txt_log.ScrollToCaret();
+        }
+
+        private void btn_end_Click(object sender, EventArgs e)
+        {
+            btn_end.Enabled = false;
+            btn_start.Enabled = true;
+            tmr_cooldown.Stop();
+            pb_cooldown.Value = 0;
+        }
+    }
+}
